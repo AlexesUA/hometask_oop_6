124734ee4b973f568ba86ef2435ddb4f75c3abf2
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-04 21:09:42 +0300

    finish task

--- src/ua/edu/cbs/lms/hometask_oop_5/Main.java
+++ src/ua/edu/cbs/lms/hometask_oop_5/Main.java
@@ -12,8 +12,27 @@ public class Main {
         System.out.println(monitor.toString());
         System.out.println(ethernetAdapter.toString());
 
+        System.out.println(monitor.hashCode());
+        System.out.println(ethernetAdapter.hashCode());
 
+        Device.Monitor newMonitor = monitor;
 
+        if(monitor.equals(newMonitor)){ // мені здавалось, що запис (monitor == newMonitor) використовує equals
+            System.out.println("Is equals.");
+        }
+        else {
+            System.out.println("Is not equals.");
+        }
+
+        Device.EthernetAdapter newEthernetAdapter= Device.EthernetAdapter.createEthernetAdapter("Intel",
+                "AE32A3FFC2", 1000, "AE-32-A3-FF-C2", (float)124.90);
+
+        if(ethernetAdapter.equals(newEthernetAdapter)){
+            System.out.println("Is equals.");
+        }
+        else {
+            System.out.println("Is not equals.");
+        }
     }
 
 
