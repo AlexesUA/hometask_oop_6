e4b9fbaa503193dd27bb7bf946ddc6d5244a97c6
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-04 19:39:15 +0300

    Created classes Device, Monitor, EthernetAdapter & fill methods

---
+++ src/ua/edu/cbs/lms/hometask_oop_5/Device.java
@@ -0,0 +1,17 @@
+package ua.edu.cbs.lms.hometask_oop_5;
+
+public class Device {
+    private String manufacturer;
+    private String serialNumber;
+    private float price;
+
+    public class Monitor{
+        private int resolutionX;
+        private int resolutionY;
+    }
+
+    public class EthernetAdapter{
+        private int spead;
+        private String mac;
+    }
+}
