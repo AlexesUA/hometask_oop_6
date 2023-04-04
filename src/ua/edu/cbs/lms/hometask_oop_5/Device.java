b1fa5348480794d74f5b816fcbdc4224c0670e4c
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-04 20:09:08 +0300

    Added Main class

--- src/ua/edu/cbs/lms/hometask_oop_5/Device.java
+++ src/ua/edu/cbs/lms/hometask_oop_5/Device.java
@@ -5,13 +5,35 @@ public class Device {
     private String serialNumber;
     private float price;
 
+    public Device(String manufacturer, String serialNumber, float price){
+        this.manufacturer = manufacturer;
+        this.serialNumber = serialNumber;
+        this.price = price;
+    }
+
     public class Monitor{
+
+        Device device;
         private int resolutionX;
         private int resolutionY;
+
+        public Monitor(String manufacturer, String serialNumber, int resolutionX, int resolutionY, float price){
+            this.device = new Device(manufacturer, serialNumber, price);
+            this.resolutionX = resolutionX;
+            this.resolutionY = resolutionY;
+        }
     }
 
     public class EthernetAdapter{
+
+        Device device;
         private int spead;
         private String mac;
+
+        public EthernetAdapter(String manufacturer, String serialNumber, int spead, String mac, float price){
+            this.device = new Device(manufacturer, serialNumber, price);
+            this.spead = spead;
+            this.mac = mac;
+        }
     }
 }
---
+++ src/ua/edu/cbs/lms/hometask_oop_5/Main.java
@@ -0,0 +1,7 @@
+package ua.edu.cbs.lms.hometask_oop_5;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
